2e5fc303de84e23ac4e323f5be96d274efd6f69a
DhruvMeduri <69319160+DhruvMeduri@users.noreply.github.com>
2023-11-02 00:02:39 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-const filterButtons = document.querySelectorAll('.filter-button');
+var map = L.map('map').setView([40.7608,-111.8910], 100);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+   // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([40.7608,-111.8910]).addTo(map);
 
-filterButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const action = button.getAttribute('data-action');
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
 
-    // Filter the data based on the selected action
-    if (action === 'block-wise') {
-      // Filter the data to show only block-wise views
-    } else if (action === 'routes-trails') {
-      // Filter the data to show only routes and trails
-    } else if (action === 'zoom-into-u-of-u') {
-      // Zoom the map into the University of Utah
-    } else if (action === 'macroscopic-view') {
-      // Filter the data to show only macroscopic views
-    }
-
-    // Change the color of the active filter button
-    filterButtons.forEach(button => {
-      button.classList.remove('active');
-    });
-    button.classList.add('active');
-  });
-});
+map.on('click', onMapClick);
